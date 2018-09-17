80360eba734ecf8c65d54ae1fe70b198199fbed3
Adam Hodges <ahodges@shipchain.io>
2018-09-17 09:04:08 -0400

    Fix bug where LoadShipment/Shipment relation isn't saved

--- apps/shipments/signals.py
+++ apps/shipments/signals.py
@@ -60,17 +60,16 @@ def shipment_post_save(sender, **kwargs):
         vault_id = rpc_client.create_vault(instance.storage_credentials_id, instance.shipper_wallet_id,
                                            instance.carrier_wallet_id)
 
-        Shipment.objects.filter(id=instance.id).update(vault_id=vault_id)
         LOG.debug(f'Created vault with vault_id {instance.vault_id}.')
-
         # Create LoadShipment entity
         # TODO: Get FundingType,ShipmentAmount,ValidUntil for use in LOAD Contract/LoadShipment
-        LoadShipment.objects.create(shipment=instance,
-                                    shipper=instance.shipper_wallet_id,
-                                    carrier=instance.carrier_wallet_id,
-                                    valid_until=Shipment.VALID_UNTIL,
-                                    funding_type=Shipment.FUNDING_TYPE,
-                                    shipment_amount=Shipment.SHIPMENT_AMOUNT)
+        load_shipment = LoadShipment.objects.create(shipment=instance,
+                                                    shipper=instance.shipper_wallet_id,
+                                                    carrier=instance.carrier_wallet_id,
+                                                    valid_until=Shipment.VALID_UNTIL,
+                                                    funding_type=Shipment.FUNDING_TYPE,
+                                                    shipment_amount=Shipment.SHIPMENT_AMOUNT)
+        Shipment.objects.filter(id=instance.id).update(vault_id=vault_id, load_data=load_shipment)
     else:
         # Update Shipment vault data
         rpc_client = ShipmentRPCClient()
