319c80461780cbe75c8defa6519f4b8a7f8712ba
James <jamesneyer@gmail.com>
2018-10-11 13:51:42 -0400

    Added tests for creating shipments both with and without locations attatched

--- tests/test_shipments.py
+++ tests/test_shipments.py
@@ -27,6 +27,7 @@ CARRIER_WALLET_ID = '3716ff65-3d03-4b65-9fd5-43d15380cff9'
 SHIPPER_WALLET_ID = '48381c16-432b-493f-9f8b-54e88a84ec0a'
 STORAGE_CRED_ID = '77b72202-5bcd-49f4-9860-bc4ec4fee07b'
 LOCATION_NAME = "Test Location Name"
+LOCATION_NAME_2 = "Second Test Location Name"
 LOCATION_CITY = 'City'
 LOCATION_STATE = 'State'
 LOCATION_NUMBER = "555-555-5555"
@@ -207,71 +208,240 @@ class ShipmentAPITests(APITestCase):
             response = self.client.post(url, {'payload': signed_data}, format='json')
             self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    #
-    # def test_create(self):
-    #     url = reverse('shipment-list', kwargs={'version': 'v1'})
-    #
-    #     # Unauthenticated request should fail with 403
-    #     response = self.client.patch(url, '{}', content_type='application/vnd.api+json')
-    #     self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-    #
-    #     parameters = {
-    #         '_vault_id': VAULT_ID,
-    #         '_carrier_wallet_id': CARRIER_WALLET_ID,
-    #         '_shipper_wallet_id': SHIPPER_WALLET_ID,
-    #         '_storage_credentials_id': STORAGE_CRED_ID,
-    #         '_async_hash': 'txHash'
-    #     }
-    #
-    #     post_data = '''
-    #         {
-    #           "data": {
-    #             "type": "Shipment",
-    #             "attributes": {
-    #               "carrier_wallet_id": "<<_carrier_wallet_id>>",
-    #               "shipper_wallet_id": "<<_shipper_wallet_id>>",
-    #               "storage_credentials_id": "<<_storage_credentials_id>>"
-    #             }
-    #           }
-    #         }
-    #     '''
-    #
-    #     # Mock RPC calls
-    #     mock_rpc_client = ShipmentRPCClient
-    #
-    #     mock_rpc_client.create_vault = mock.Mock(return_value=parameters['_vault_id'])
-    #     mock_rpc_client.add_shipment_data = mock.Mock(return_value={'hash': 'txHash'})
-    #     mock_rpc_client.create_shipment_transaction = mock.Mock(return_value=('version', {}))
-    #     mock_rpc_client.sign_transaction = mock.Mock(return_value=({}, 'txHash'))
-    #     mock_rpc_client.send_transaction = mock.Mock(return_value={
-    #         "blockHash": "0xccb595947a121e37df8bf689c3f88c6d9c7fb56070c9afda38551540f9e231f7",
-    #         "blockNumber": 15,
-    #         "contractAddress": None,
-    #         "cumulativeGasUsed": 138090,
-    #         "from": "0x13b1eebb31a1aa2ecaa2ad9e7455df2f717f2143",
-    #         "gasUsed": 138090,
-    #         "logs": [],
-    #         "logsBloom": "0x0000000000",
-    #         "status": True,
-    #         "to": "0x25ff5dc79a7c4e34254ff0f4a19d69e491201dd3",
-    #         "transactionHash": parameters['_async_hash'],
-    #         "transactionIndex": 0
-    #     })
-    #
-    #     # Authenticated request should succeed
-    #     self.set_user(self.user_1)
-    #
-    #     post_data = replace_variables_in_string(post_data, parameters)
-    #
-    #     response = self.client.post(url, post_data, content_type='application/vnd.api+json')
-    #     print(response.content)
-    #
-    #     response_data = response.json()
-    #     self.assertEqual(response_data['data']['attributes']['carrier_wallet_id'], parameters['_carrier_wallet_id'])
-    #     self.assertEqual(response_data['data']['attributes']['shipper_wallet_id'], parameters['_shipper_wallet_id'])
-    #     self.assertEqual(response_data['data']['attributes']['storage_credentials_id'], parameters['_storage_credentials_id'])
-    #     self.assertEqual(response_data['data']['attributes']['vault_id'], parameters['_vault_id'])
-    #     self.assertEqual(response_data['data']['meta']['transaction_id'], parameters['_async_hash'])
+    def test_create(self):
+        url = reverse('shipment-list', kwargs={'version': 'v1'})
+
+        # Unauthenticated request should fail with 403
+        response = self.client.patch(url, '{}', content_type='application/vnd.api+json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        parameters = {
+            '_vault_id': VAULT_ID,
+            '_carrier_wallet_id': CARRIER_WALLET_ID,
+            '_shipper_wallet_id': SHIPPER_WALLET_ID,
+            '_storage_credentials_id': STORAGE_CRED_ID,
+            '_async_hash': 'txHash'
+        }
+
+        post_data = '''
+            {
+              "data": {
+                "type": "Shipment",
+                "attributes": {
+                  "carrier_wallet_id": "<<_carrier_wallet_id>>",
+                  "shipper_wallet_id": "<<_shipper_wallet_id>>",
+                  "storage_credentials_id": "<<_storage_credentials_id>>"
+                }
+              }
+            }
+        '''
+
+        # Mock RPC calls
+        mock_shipment_rpc_client = ShipmentRPCClient
+
+        mock_shipment_rpc_client.create_vault = mock.Mock(return_value=parameters['_vault_id'])
+        mock_shipment_rpc_client.add_shipment_data = mock.Mock(return_value={'hash': 'txHash'})
+        mock_shipment_rpc_client.create_shipment_transaction = mock.Mock(return_value=('version', {}))
+        mock_shipment_rpc_client.create_shipment_transaction.__qualname__ = 'ShipmentRPCClient.create_shipment_transaction'
+        mock_shipment_rpc_client.sign_transaction = mock.Mock(return_value=({}, 'txHash'))
+        mock_shipment_rpc_client.update_vault_hash_transaction = mock.Mock(return_value=({}))
+        mock_shipment_rpc_client.update_vault_hash_transaction.__qualname__ = 'ShipmentRPCClient.update_vault_hash_transaction'
+        mock_shipment_rpc_client.send_transaction = mock.Mock(return_value={
+            "blockHash": "0xccb595947a121e37df8bf689c3f88c6d9c7fb56070c9afda38551540f9e231f7",
+            "blockNumber": 15,
+            "contractAddress": None,
+            "cumulativeGasUsed": 138090,
+            "from": "0x13b1eebb31a1aa2ecaa2ad9e7455df2f717f2143",
+            "gasUsed": 138090,
+            "logs": [],
+            "logsBloom": "0x0000000000",
+            "status": True,
+            "to": "0x25ff5dc79a7c4e34254ff0f4a19d69e491201dd3",
+            "transactionHash": parameters['_async_hash'],
+            "transactionIndex": 0
+        })
+
+        # Authenticated request should succeed
+        self.set_user(self.user_1)
+
+        post_data = replace_variables_in_string(post_data, parameters)
+
+        response = self.client.post(url, post_data, content_type='application/vnd.api+json')
+        print(response.json()['included'])
+
+        response_data = response.json()['data']
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+        self.assertEqual(response_data['attributes']['carrier_wallet_id'], parameters['_carrier_wallet_id'])
+        self.assertEqual(response_data['attributes']['shipper_wallet_id'], parameters['_shipper_wallet_id'])
+        self.assertEqual(response_data['attributes']['storage_credentials_id'], parameters['_storage_credentials_id'])
+        self.assertEqual(response_data['attributes']['vault_id'], parameters['_vault_id'])
+        self.assertIsNotNone(response_data['meta']['async_job_id'])
+
+    @httpretty.activate
+    def test_create_with_location(self):
+        url = reverse('shipment-list', kwargs={'version': 'v1'})
+
+        # Unauthenticated request should fail with 403
+        response = self.client.patch(url, '{}', content_type='application/vnd.api+json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        parameters = {
+            '_vault_id': VAULT_ID,
+            '_carrier_wallet_id': CARRIER_WALLET_ID,
+            '_shipper_wallet_id': SHIPPER_WALLET_ID,
+            '_storage_credentials_id': STORAGE_CRED_ID,
+            '_async_hash': 'txHash',
+            '_ship_from_location_city': LOCATION_CITY,
+            '_ship_from_location_state': LOCATION_STATE,
+            '_ship_from_location_name': LOCATION_NAME,
+            '_ship_to_location_name': LOCATION_NAME_2
+        }
+
+        one_location, content_type = create_form_content({'ship_from_location.name': LOCATION_NAME,
+                                                         'ship_from_location.city': LOCATION_CITY,
+                                                         'ship_from_location.state': LOCATION_STATE,
+                                                         'carrier_wallet_id': CARRIER_WALLET_ID,
+                                                         'shipper_wallet_id': SHIPPER_WALLET_ID,
+                                                         'storage_credentials_id': STORAGE_CRED_ID
+                                                         })
+
+        two_locations, content_type = create_form_content({'ship_from_location.name': LOCATION_NAME,
+                                                           'ship_from_location.city': LOCATION_CITY,
+                                                           'ship_from_location.state': LOCATION_STATE,
+                                                           'ship_to_location.name': LOCATION_NAME_2,
+                                                           'ship_to_location.city': LOCATION_CITY,
+                                                           'ship_to_location.state': LOCATION_STATE,
+                                                           'carrier_wallet_id': CARRIER_WALLET_ID,
+                                                           'shipper_wallet_id': SHIPPER_WALLET_ID,
+                                                           'storage_credentials_id': STORAGE_CRED_ID
+                                                           })
+
+        # Mock RPC calls
+        mock_shipment_rpc_client = ShipmentRPCClient
+
+        mock_shipment_rpc_client.create_vault = mock.Mock(return_value=parameters['_vault_id'])
+        mock_shipment_rpc_client.add_shipment_data = mock.Mock(return_value={'hash': 'txHash'})
+        mock_shipment_rpc_client.create_shipment_transaction = mock.Mock(return_value=('version', {}))
+        mock_shipment_rpc_client.create_shipment_transaction.__qualname__ = 'ShipmentRPCClient.create_shipment_transaction'
+        mock_shipment_rpc_client.sign_transaction = mock.Mock(return_value=({}, 'txHash'))
+        mock_shipment_rpc_client.update_vault_hash_transaction = mock.Mock(return_value=({}))
+        mock_shipment_rpc_client.update_vault_hash_transaction.__qualname__ = 'ShipmentRPCClient.update_vault_hash_transaction'
+        mock_shipment_rpc_client.send_transaction = mock.Mock(return_value={
+            "blockHash": "0xccb595947a121e37df8bf689c3f88c6d9c7fb56070c9afda38551540f9e231f7",
+            "blockNumber": 15,
+            "contractAddress": None,
+            "cumulativeGasUsed": 138090,
+            "from": "0x13b1eebb31a1aa2ecaa2ad9e7455df2f717f2143",
+            "gasUsed": 138090,
+            "logs": [],
+            "logsBloom": "0x0000000000",
+            "status": True,
+            "to": "0x25ff5dc79a7c4e34254ff0f4a19d69e491201dd3",
+            "transactionHash": parameters['_async_hash'],
+            "transactionIndex": 0
+        })
+
+        google_obj = {'results': [{'address_components': [{'types': []}], 'geometry': {'location': {'lat': 12, 'lng': 23}}}]}
+        mapbox_obj = {'features': [{'place_type': [{'types': []}], 'geometry': {'coordinates': [23, 12]}}]}
+
+        httpretty.register_uri(httpretty.GET, google_url, body=json.dumps(google_obj))
+        httpretty.register_uri(httpretty.GET, mapbox_url, body=json.dumps(mapbox_obj))
+
+        # Authenticated request should succeed using mapbox (if exists)
+        self.set_user(self.user_1)
+
+        response = self.client.post(url, one_location, content_type=content_type)
+        response_data = response.json()['data']
+        response_included = response.json()['included']
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+        self.assertEqual(response_data['attributes']['carrier_wallet_id'], parameters['_carrier_wallet_id'])
+        self.assertEqual(response_data['attributes']['shipper_wallet_id'], parameters['_shipper_wallet_id'])
+        self.assertEqual(response_data['attributes']['storage_credentials_id'],
+                         parameters['_storage_credentials_id'])
+        self.assertEqual(response_data['attributes']['vault_id'], parameters['_vault_id'])
+        self.assertIsNotNone(response_data['meta']['async_job_id'])
+
+        self.assertIsNotNone(response_data['relationships']['ship_from_location']['data'])
+        self.assertEqual(len(response_included), 2)
+        self.assertEqual(response_included[1]['attributes']['city'], parameters['_ship_from_location_city'])
+        self.assertEqual(response_included[1]['attributes']['state'], parameters['_ship_from_location_state'])
+        self.assertEqual(response_included[1]['attributes']['geometry']['coordinates'], [12.0, 23.0])
+
+        # Authenticated request should succeed using mapbox in creating two locations (if exists)
+        self.set_user(self.user_1)
+
+        response = self.client.post(url, two_locations, content_type=content_type)
+        response_data = response.json()['data']
+        response_included = response.json()['included']
+        print(response_included)
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+        self.assertEqual(response_data['attributes']['carrier_wallet_id'], parameters['_carrier_wallet_id'])
+        self.assertEqual(response_data['attributes']['shipper_wallet_id'], parameters['_shipper_wallet_id'])
+        self.assertEqual(response_data['attributes']['storage_credentials_id'],
+                         parameters['_storage_credentials_id'])
+        self.assertEqual(response_data['attributes']['vault_id'], parameters['_vault_id'])
+        self.assertIsNotNone(response_data['meta']['async_job_id'])
+
+        self.assertIsNotNone(response_data['relationships']['ship_from_location']['data'])
+        self.assertEqual(response_included[1]['attributes']['city'], parameters['_ship_from_location_city'])
+        self.assertEqual(response_included[1]['attributes']['state'], parameters['_ship_from_location_state'])
+        self.assertEqual(response_included[1]['attributes']['name'], parameters['_ship_from_location_name'])
+        self.assertEqual(response_included[1]['attributes']['geometry']['coordinates'], [12.0, 23.0])
+
+        self.assertIsNotNone(response_data['relationships']['ship_to_location']['data'])
+        self.assertEqual(len(response_included), 3)
+        self.assertEqual(response_included[2]['attributes']['city'], parameters['_ship_from_location_city'])
+        self.assertEqual(response_included[2]['attributes']['state'], parameters['_ship_from_location_state'])
+        self.assertEqual(response_included[2]['attributes']['name'], parameters['_ship_to_location_name'])
+        self.assertEqual(response_included[2]['attributes']['geometry']['coordinates'], [12.0, 23.0])
+
+        # Authenticated request should succeed using google
+        mapbox_access_token = None
+        self.set_user(self.user_1)
+
+        response = self.client.post(url, one_location, content_type=content_type)
+        response_data = response.json()['data']
+        response_included = response.json()['included']
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+        self.assertEqual(response_data['attributes']['carrier_wallet_id'], parameters['_carrier_wallet_id'])
+        self.assertEqual(response_data['attributes']['shipper_wallet_id'], parameters['_shipper_wallet_id'])
+        self.assertEqual(response_data['attributes']['storage_credentials_id'],
+                         parameters['_storage_credentials_id'])
+        self.assertEqual(response_data['attributes']['vault_id'], parameters['_vault_id'])
+        self.assertIsNotNone(response_data['meta']['async_job_id'])
+
+        self.assertIsNotNone(response_data['relationships']['ship_from_location']['data'])
+        self.assertEqual(len(response_included), 2)
+        self.assertEqual(response_included[1]['attributes']['city'], parameters['_ship_from_location_city'])
+        self.assertEqual(response_included[1]['attributes']['state'], parameters['_ship_from_location_state'])
+        self.assertEqual(response_included[1]['attributes']['geometry']['coordinates'], [12.0, 23.0])
+
+        # Authenticated request should succeed using google in creating two locations
+        self.set_user(self.user_1)
+
+        response = self.client.post(url, two_locations, content_type=content_type)
+        response_data = response.json()['data']
+        response_included = response.json()['included']
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+        self.assertEqual(response_data['attributes']['carrier_wallet_id'], parameters['_carrier_wallet_id'])
+        self.assertEqual(response_data['attributes']['shipper_wallet_id'], parameters['_shipper_wallet_id'])
+        self.assertEqual(response_data['attributes']['storage_credentials_id'],
+                         parameters['_storage_credentials_id'])
+        self.assertEqual(response_data['attributes']['vault_id'], parameters['_vault_id'])
+        self.assertIsNotNone(response_data['meta']['async_job_id'])
+
+        self.assertIsNotNone(response_data['relationships']['ship_from_location']['data'])
+        self.assertEqual(response_included[1]['attributes']['city'], parameters['_ship_from_location_city'])
+        self.assertEqual(response_included[1]['attributes']['state'], parameters['_ship_from_location_state'])
+        self.assertEqual(response_included[1]['attributes']['name'], parameters['_ship_from_location_name'])
+        self.assertEqual(response_included[1]['attributes']['geometry']['coordinates'], [12.0, 23.0])
+
+        self.assertIsNotNone(response_data['relationships']['ship_to_location']['data'])
+        self.assertEqual(len(response_included), 3)
+        self.assertEqual(response_included[2]['attributes']['city'], parameters['_ship_from_location_city'])
+        self.assertEqual(response_included[2]['attributes']['state'], parameters['_ship_from_location_state'])
+        self.assertEqual(response_included[2]['attributes']['name'], parameters['_ship_to_location_name'])
+        self.assertEqual(response_included[2]['attributes']['geometry']['coordinates'], [12.0, 23.0])
 
     # def test_get_device_request_url(self):
     #     from conf.test_settings import PROFILES_URL
