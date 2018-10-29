8a06836435b2580508583b71d3febfc2e4a65464
Adam Hodges <ahodges@shipchain.io>
2018-10-29 17:26:38 -0400

    Move away from trying to use a django channels middleware

--- apps/authentication.py
+++ apps/authentication.py
@@ -14,9 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+from asgiref import sync
 from collections import namedtuple
 from django.conf import settings
-from channels.auth import BaseMiddleware, UserLazyObject
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from rest_framework import exceptions
 from rest_framework.permissions import BasePermission
@@ -42,56 +42,22 @@ class AuthenticatedUser:
         return False
 
 
+def passive_credentials_authentication(payload):
+    if 'sub' not in payload:
+        raise exceptions.AuthenticationFailed('Invalid payload.')
+
+    payload['pk'] = payload['sub']
+    payload = namedtuple("User", payload.keys())(*payload.values())
+    payload = api_settings.JWT_PAYLOAD_HANDLER(payload)
+
+    user = AuthenticatedUser(payload)
+
+    return user
+
+
 class PassiveJSONWebTokenAuthentication(JSONWebTokenAuthentication):
     def authenticate_credentials(self, payload):
-        if 'sub' not in payload:
-            raise exceptions.AuthenticationFailed('Invalid payload.')
-
-        payload['pk'] = payload['sub']
-        payload = namedtuple("User", payload.keys())(*payload.values())
-        payload = api_settings.JWT_PAYLOAD_HANDLER(payload)
-
-        user = AuthenticatedUser(payload)
-
-        return user
-
-
-class ChannelsJWTMiddleware(BaseMiddleware, PassiveJSONWebTokenAuthentication):
-    """
-    Middleware which populates scope["user"] from a JWT.
-    """
-
-    def populate_scope(self, scope):
-        # Add it to the scope if it's not there already
-        if "user" not in scope:
-            scope["user"] = UserLazyObject()
-
-    async def resolve_scope(self, scope):
-        if "jwt" not in scope:
-            scope["jwt"] = self._get_jwt_from_subprotocols(scope)
-        scope["user"]._wrapped = self._get_user(scope)
-
-    def _get_jwt_from_subprotocols(self, scope):
-        jwt = None
-
-        # Initially parse jwt out of subprotocols
-        for protocol in scope['subprotocols']:
-            if protocol.startswith('base64.jwt.'):
-                jwt = protocol.split('base64.jwt.')[1]
-
-        return jwt
-
-    def _get_user(self, scope):
-        user = None
-
-        try:
-            if scope['jwt']:
-                payload = jwt_decode_handler(scope['jwt'])
-                user = self.authenticate_credentials(payload)
-        except Exception:
-            pass
-
-        return user
+        return passive_credentials_authentication(payload)
 
 
 class AsyncJsonAuthConsumer(AsyncJsonWebsocketConsumer):
@@ -99,9 +65,7 @@ class AsyncJsonAuthConsumer(AsyncJsonWebsocketConsumer):
     Consumer that requires user to be present in the scope.
     """
     async def connect(self):
-        if not self.scope['user']._wrapped:
-            await self.close()
-        else:
+        if await self._authenticate():
             await self.channel_layer.group_add("jobs", self.channel_name)
             await self.accept('base64.authentication.jwt')
 
@@ -110,23 +74,51 @@ class AsyncJsonAuthConsumer(AsyncJsonWebsocketConsumer):
         await super().disconnect(code)
 
     async def receive(self, text_data=None, bytes_data=None, **kwargs):
-        if not self.scope['user']._wrapped:
-            await self.close()
-        else:
+        if await self._authenticate():
             if text_data:
                 json = await self.decode_json(text_data)
                 if 'event' in json and 'data' in json and json['event'] == 'refresh_jwt':
-                    self.scope['jwt'] = text_data['data']
+                    self.scope['jwt'] = json['data']
                 else:
                     await self.receive_json(json, **kwargs)
             else:
                 raise ValueError("No text section for incoming WebSocket frame!")
 
     async def send(self, text_data=None, bytes_data=None, close=False):
-        if not self.scope['user']._wrapped:
-            await self.close()
-        else:
+        if await self._authenticate():
             await super().send(text_data, bytes_data, close)
+
+    async def _authenticate(self):
+        if "jwt" not in self.scope:
+            self.scope["jwt"] = await self._get_jwt_from_subprotocols()
+        self.scope["user"] = await self._get_user()
+        if not self.scope['user'] or ('user_id' in self.scope['url_route']['kwargs'] and
+                                      self.scope['url_route']['kwargs']['user_id'] != self.scope['user'].id):
+            await self.close()
+            return False
+        return True
+
+    async def _get_jwt_from_subprotocols(self):
+        jwt = None
+
+        # Initially parse jwt out of subprotocols
+        for protocol in self.scope['subprotocols']:
+            if protocol.startswith('base64.jwt.'):
+                jwt = protocol.split('base64.jwt.')[1]
+
+        return jwt
+
+    async def _get_user(self):
+        user = None
+
+        try:
+            if self.scope['jwt']:
+                payload = await sync.sync_to_async(jwt_decode_handler)(self.scope['jwt'])
+                user = await sync.sync_to_async(passive_credentials_authentication)(payload)
+        except Exception:
+            pass
+
+        return user
 
 
 class EngineRequest(BasePermission):
