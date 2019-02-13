1e4c6e80d2bbb6215db99dec7bf0ab1532d9f55b
James <jamesneyer@gmail.com>
2019-02-13 10:57:45 -0500

    Add regex for nmfc_class

--- apps/shipments/models.py
+++ apps/shipments/models.py
@@ -248,8 +248,9 @@ class Shipment(models.Model):
     seal_number = models.CharField(max_length=255, blank=True, null=True)
     is_master_bol = models.BooleanField(default=False)
 
-    nmfc_number = models.CharField(max_length=255, blank=True, null=True)
-    nmfc_class = models.CharField(max_length=255, blank=True, null=True)
+    nmfc_regex = RegexValidator(regex=r'^1[015]0$|^1[27]5$|^2[05]0$|^[345]00$|^[567]0$|^[568]5$|^92\.5$|^77\.5$',
+                                message="Invalid phone number.")
+    nmfc_class = models.CharField(validators=[nmfc_regex], max_length=4, blank=True, null=True)
 
     is_hazmat = models.BooleanField(default=False)
 
