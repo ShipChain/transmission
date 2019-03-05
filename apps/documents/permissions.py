a4f5892f9d73e125d6fabbf2e4b4d4dc257f1d64
Clovis <diclovis@gmail.com>
2019-03-05 12:12:05 -0500

    Uniformize JWT request header syntaxe

--- apps/documents/permissions.py
+++ apps/documents/permissions.py
@@ -4,7 +4,7 @@ from apps.authentication import get_jwt_from_request
 from apps.permissions import has_owner_access
 
 
-PROFILES_URL = f'{settings.PROFILES_URL}/api/v1/wallet/'
+PROFILES_URL = f'{settings.PROFILES_URL}/api/v1/wallet'
 
 
 class UserHasPermission(permissions.BasePermission):
@@ -19,32 +19,26 @@ class UserHasPermission(permissions.BasePermission):
             Custom permission for carrier documents management access
             """
             response = settings.REQUESTS_SESSION.get(f'{PROFILES_URL}/{obj.shipment.carrier_wallet_id}/',
-                                                     headers={'Authorization': get_jwt_from_request(request)})
+                                                     headers={'Authorization': f'JWT {get_jwt_from_request(request)}'})
 
-            if response.status_code != status.HTTP_200_OK:
-                return False
-            return True
+            return response.status_code == status.HTTP_200_OK
 
         def is_moderator():
             """
             Custom permission for moderator documents management access
             """
             response = settings.REQUESTS_SESSION.get(f'{PROFILES_URL}/{obj.shipment.moderator_wallet_id}/',
-                                                     headers={'Authorization': get_jwt_from_request(request)})
+                                                     headers={'Authorization': f'JWT {get_jwt_from_request(request)}'})
 
-            if response.status_code != status.HTTP_200_OK:
-                return False
-            return True
+            return response.status_code == status.HTTP_200_OK
 
         def is_shipper():
             """
             Custom permission for shipper documents management access
             """
             response = settings.REQUESTS_SESSION.get(f'{PROFILES_URL}/{obj.shipment.shipper_wallet_id}/',
-                                                     headers={'Authorization': get_jwt_from_request(request)})
+                                                     headers={'Authorization': f'JWT {get_jwt_from_request(request)}'})
 
-            if response.status_code != status.HTTP_200_OK:
-                return False
-            return True
+            return response.status_code == status.HTTP_200_OK
 
         return has_owner_access(request, obj) or is_shipper() or is_carrier() or is_moderator()
