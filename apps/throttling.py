c8f8dc4b4fa736b71a1c0215db888dfe333dfa7c
James Neyer <jamesneyer@gmail.com>
2019-07-03 15:17:48 -0400

    Move logging calls to throttle_success/failure

--- apps/throttling.py
+++ apps/throttling.py
@@ -8,13 +8,16 @@ from rest_framework import throttling
 LOG = logging.getLogger('transmission')
 
 
-# pylint: disable=attribute-defined-outside-init, super-init-not-called
 class MonthlyRateThrottle(throttling.SimpleRateThrottle):
     def __init__(self):
         month = datetime.now().month
         year = datetime.now().year
-        self.duration = monthrange(year, month)[1] * 86400
         # Rate limit is dynamically extracted from JWT
+        self.duration = monthrange(year, month)[1] * 86400
+        # Initialize here to avoid pylint error
+        self.key = None
+        self.history = None
+        self.now = None
 
     def get_cache_key(self, request, view):
         if not request.user.is_authenticated:
@@ -33,25 +36,39 @@ class MonthlyRateThrottle(throttling.SimpleRateThrottle):
         if not self.key:
             return True
 
-        self.history = self.cache.get(cache_key, [])
-        self.now = datetime.now().timestamp()
         self.num_requests = request.user.token.get('monthly_rate_limit', None)
 
         if not self.num_requests:
             return True
 
+        self.history = self.cache.get(cache_key, [])
+        self.now = datetime.now().timestamp()
+
         # Drop any requests from the history which have now passed the
         # throttle duration
         while self.history and self.history[-1] <= self.now - self.duration:
             self.history.pop()
-        if not self.num_requests:
-            log_metric('transmission.info', tags={'method': 'throttling.MonthlyRateThrottle', 'module': __name__,
-                                                  'organization_id': self.key, 'success': True})
-            return self.throttle_success()
+
         if len(self.history) >= self.num_requests:
-            log_metric('transmission.info', tags={'method': 'throttling.MonthlyRateThrottle', 'module': __name__,
-                                                  'organization_id': self.key, 'success': False})
             return self.throttle_failure()
+
+        return self.throttle_success()
+
+    def throttle_success(self):
+        """
+        Inserts the current request's timestamp along with the key
+        into the cache.
+        """
         log_metric('transmission.info', tags={'method': 'throttling.MonthlyRateThrottle', 'module': __name__,
                                               'organization_id': self.key, 'success': True})
-        return self.throttle_success()
+        self.history.insert(0, self.now)
+        self.cache.set(self.key, self.history, self.duration)
+        return True
+
+    def throttle_failure(self):
+        """
+        Called when a request to the API has failed due to throttling.
+        """
+        log_metric('transmission.info', tags={'method': 'throttling.MonthlyRateThrottle', 'module': __name__,
+                                              'organization_id': self.key, 'success': False})
+        return False
