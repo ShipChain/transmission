ee83cdbc57cdf6f642ca4b2e5696194d6035ee94
Adam Hodges <ahodges@shipchain.io>
2019-07-24 16:31:50 -0400

    Add shipment filters for state

--- apps/shipments/filters.py
+++ apps/shipments/filters.py
@@ -11,14 +11,14 @@ class ShipmentFilter(filters.filterset.FilterSet):
 
     class Meta:
         model = Shipment
-        fields = ['state', 'mode_of_transport_code', 'ship_from_location__name', 'ship_from_location__city',
+        fields = ['mode_of_transport_code', 'ship_from_location__name', 'ship_from_location__city',
                   'ship_from_location__address_1', 'ship_from_location__postal_code', 'ship_from_location__country',
                   'ship_to_location__name', 'ship_to_location__city', 'ship_to_location__state',
                   'ship_to_location__address_1', 'ship_to_location__postal_code', 'ship_to_location__country',
                   'final_destination_location__name', 'final_destination_location__city', 'ship_from_location',
                   'final_destination_location__state', 'final_destination_location__address_1', 'ship_to_location',
                   'final_destination_location__postal_code', 'final_destination_location__country',
-                  'final_destination_location', 'ship_from_location__state',]
+                  'final_destination_location', 'ship_from_location__state', 'state', 'exception', 'delayed']
 
 
 class HistoricalShipmentFilter(filters.filterset.FilterSet):
