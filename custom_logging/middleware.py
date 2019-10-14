f890266ad35fdd2f84f14dba6e6f8b94463e26e2
Clovis <diclovis@gmail.com>
2019-10-14 14:15:18 -0400

    Simplify UserOrganizationMiddleware

--- custom_logging/middleware.py
+++ custom_logging/middleware.py
@@ -17,7 +17,6 @@ limitations under the License.
 import threading
 
 from django.conf import settings
-from django.core.exceptions import ObjectDoesNotExist
 from django.utils.deprecation import MiddlewareMixin
 
 CURRENT_THREAD = threading.current_thread()
@@ -30,11 +29,10 @@ class UserOrganizationMiddleware(MiddlewareMixin):
         from apps.authentication import PASSIVE_JWT_AUTHENTICATION, passive_credentials_auth
 
         user_id, org_id = None, None
-        req_auth = request.headers._store.get('authorization')  # pylint: disable=protected-access
-        username = request.POST.get('username')
+        header = PASSIVE_JWT_AUTHENTICATION.get_header(request)
 
-        if req_auth:
-            raw_token = req_auth[-1].split(' ')[-1]
+        if header:
+            raw_token = PASSIVE_JWT_AUTHENTICATION.get_raw_token(header)
 
             if settings.ENVIRONMENT.lower() in ('int', 'local'):
                 from rest_framework_simplejwt.tokens import UntypedToken
@@ -42,23 +40,10 @@ class UserOrganizationMiddleware(MiddlewareMixin):
                 # unhandled exception to be thrown to the client
                 token_user = PASSIVE_JWT_AUTHENTICATION.get_user(UntypedToken(raw_token, verify=False))
             else:
-                token_user = passive_credentials_auth(req_auth[-1].split(' ')[-1])
+                token_user = passive_credentials_auth(raw_token)
 
             org_id = token_user.token.payload.get('organization_id')
             user_id = token_user.id
-
-        elif username:
-            user = None
-            try:
-                from apps.profiles.models import User
-                user = User.objects.get(username=username)
-            except (ImportError, ObjectDoesNotExist):
-                pass
-
-            if user is not None:
-                user_id = user.id
-                organization = user.organizations.first()
-                org_id = organization.id if organization else None
 
         setattr(CURRENT_THREAD, 'organization_id', org_id)
         setattr(CURRENT_THREAD, 'user_id', user_id)
