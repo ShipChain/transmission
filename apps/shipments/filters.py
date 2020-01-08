dd655567b19b6f9face48efdbd14262f5e00dea0
Clovis <diclovis@gmail.com>
2020-01-08 15:36:03 -0500

    Update shipment search fields with location contact fields

--- apps/shipments/filters.py
+++ apps/shipments/filters.py
@@ -40,9 +40,12 @@ SHIPMENT_SEARCH_FIELDS = (
     'shippers_reference', 'forwarders_reference',
     'ship_from_location__name', 'ship_from_location__city', 'ship_from_location__address_1',
     'ship_from_location__postal_code', 'ship_from_location__country', 'ship_from_location__state',
+    'ship_from_location__contact_name', 'ship_from_location__contact_email',
     'ship_to_location__name', 'ship_to_location__city', 'ship_to_location__address_1',
     'ship_to_location__postal_code', 'ship_to_location__country', 'ship_to_location__state',
+    'ship_to_location__contact_name', 'ship_to_location__contact_email',
     'final_destination_location__name', 'final_destination_location__city', 'final_destination_location__address_1',
     'final_destination_location__postal_code', 'final_destination_location__country',
-    'final_destination_location__state',
+    'final_destination_location__state', 'final_destination_location__contact_name',
+    'final_destination_location__contact_email',
 )
